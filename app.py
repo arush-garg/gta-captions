c084b5512c9e0d5bed7ae2f37cb22517a061362f
Arush Garg <arushg0312@gmail.com>
2025-08-09 13:26:39 -0700

    Bigger model fine-tune

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PIL import Image
 def load_model():
     with st.spinner("Loading model..."):
         model = AutoModelForCausalLM.from_pretrained("Techno03/gta-captioner")
-        processor = AutoProcessor.from_pretrained("microsoft/git-base")
+        processor = AutoProcessor.from_pretrained("microsoft/git-large")
     
     if torch.cuda.is_available():
         model.to("cuda")
@@ -30,7 +30,7 @@ with col1:
 
 with col2:
     st.write("Unsure? Click Generate Caption to try this example")
-    st.image("example/image1.png", caption="Example Image", use_container_width=True)
+    st.image("example/image2.png", caption="Example Image", use_container_width=True)
 
 generate_button = st.button("Generate Caption")
 
@@ -39,7 +39,7 @@ if generate_button:
         image = Image.open(uploaded_file).convert("RGB")
         st.image(image, caption="Uploaded Image", use_container_width=True)
     else:
-        image = Image.open("example/image1.png").convert("RGB")
+        image = Image.open("example/image2.png").convert("RGB")
 
     inputs = st.session_state.processor(images=image, return_tensors="pt").to(st.session_state.model.device)
     
